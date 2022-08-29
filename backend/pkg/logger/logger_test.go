434b8e52375c29fcc126a668adb96777450eea61
mudi25 <42921505+mudi25@users.noreply.github.com>
2022-08-29 18:10:19 +0800

    fix workflow pkg

---
+++ backend/pkg/logger/logger_test.go
@@ -0,0 +1,19 @@
+package logger_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/mudi25/monorepo-ci-cd/backend/pkg/logger"
+)
+
+func TestMain(m *testing.M) {
+	m.Run()
+}
+func TestError(t *testing.T) {
+	logger.Error(fmt.Errorf("invalid argument"))
+	logger.Error(nil)
+}
+func TestInfo(t *testing.T) {
+	logger.Info("hello")
+}
